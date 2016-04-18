2ce1916eeb15a4ea61e371ea9bc1520b3e9b4960
Aditya Gaykar <adityagaykar@gmail.com>
2016-04-18 14:51:06 +0530

    adding user db backup

--- utils/servers.js
+++ utils/servers.js
@@ -6,7 +6,28 @@ var servers = {
 	user_app_server: {
 		hostname: "localhost",
 		port: 3001
+	},
+	logic_server:{
+		hostname: "localhost",
+		port: 3002
+	},
+	event_server:{
+		hostname: "localhost",
+		port: 3003
+	},
+	notification_server:{
+		hostname: "localhost",
+		port: 3004
+	},
+	data_server: {
+		hostname: "localhost",
+		port: 3005
+	},
+	data_listener : {
+		hostname: "localhost",
+		port: 3006
 	}
-};
+
+}
 
 module.exports = servers;
